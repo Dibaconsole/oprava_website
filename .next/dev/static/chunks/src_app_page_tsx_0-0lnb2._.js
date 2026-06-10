927f7d04e14bcc5814c56b8cf8980f1d671ebb77
MMekinda <emekinda@dibasolutions.com>
2026-06-10 08:18:21 +0100

    Remove .next from version control

--- .next/dev/static/chunks/src_app_page_tsx_0-0lnb2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_components_0x8r3tw._.js","static/chunks/node_modules_next_0aj6m7e._.js"],
-    source: "dynamic"
-});
